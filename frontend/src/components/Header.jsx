c999334d239e5f6a9f851c5f70824b7d06290999
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-06-30 21:03:49 -0300

    feat: add MUI material

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,26 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
+
 export default function Header() {
-  return <header>
-    Cabecera
-  </header>;
+  return <AppBar position="static">
+    <Toolbar>
+      <IconButton color="inherit">
+        <MenuIcon />
+      </IconButton>
+
+      <img class="header-logo" src="/minetech.png" alt="Logo" />
+
+      <Typography variant="h6" component="div">
+        AI Desk
+      </Typography>
+
+      <IconButton color="inherit">
+        <AccountCircle />
+      </IconButton>
+    </Toolbar>
+  </AppBar>;
 }
