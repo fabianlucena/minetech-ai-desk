70cd3465d39a7fe238db96ec3c564be015036a32
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-07-04 23:54:30 -0300

    feat: add column hasPassword to Usersr grid

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -29,6 +29,11 @@ export default function Usuarios() {
       renderCell: ({value}) => value ? '✔️' : '❌',
     },
     {
+      field: 'hasPassword',
+      headerName: 'Contraseña',
+      renderCell: ({value}) => value ? '✔️' : '❌',
+    },
+    {
       field: 'canLogin',
       headerName: 'Conectable',
       renderCell: ({value}) => value ? '✔️' : '❌',
