b4ac537419dea5a0ce8b9bad96c9711cd89f145b
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-07-06 11:52:07 -0300

    feat: implement dynamic route configuration and add technician management routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,39 @@
 import { Router } from 'express';
-import helloRouter from './hello.routes.js';
-import usersRouter from './users.routes.js';
-import loginRouter from './login.routes.js';
-import autoLoginRouter from './auto-login.routes.js';
-import techniciansRouter from './technician.routes.js';
+import fs from 'fs';
+import path from 'path';
+import logger from '../logger.js';
+import { dirname } from 'path';
+import { fileURLToPath, pathToFileURL  } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const router = Router();
 
-router.use('/hello', helloRouter);
-router.use('/users', usersRouter);
-router.use('/login', loginRouter);
-router.use('/auto-login', autoLoginRouter);
-router.use('/technicians', techniciansRouter);
+export function snakeToCamel(str) {
+  return str.replace(/[_-]([a-z])/g, (_, letter) => letter.toUpperCase());
+}
+
+try {
+  logger.info('🛢️  Configurando rutas');
+
+  const files = fs.readdirSync(__dirname)
+    .filter(file => file.endsWith('.routes.js') && file !== 'index.js');
+
+  for (const file of files) {
+    const endPointPath = '/' + file.replace('.routes.js', '');
+
+    logger.info(`    ${endPointPath} -> ${file}`);
+    const fullPath = path.join(__dirname, file);
+    const fileUrl = pathToFileURL(fullPath).href;
+    const { default: routes } = await import(fileUrl);
+    router.use(endPointPath, routes);
+  }
+
+  logger.info('🛢️  Rutas configuradas OK ✔️');
+} catch (error) {
+  logger.error('❌ Error al configurar rutas:', error);
+  process.exit(1);
+}
 
 export default router;
