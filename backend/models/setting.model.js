1142ffe7d64eefb08d862defd002b95c929394aa
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-07-25 23:53:32 -0300

    feat: add settings model

--- backend/models/setting.model.js
+++ backend/models/setting.model.js
@@ -1,8 +1,7 @@
 import { DataTypes } from 'sequelize';
-import { clientStatusValues } from '../categories/client_status.js';
 
 export default (sequelize) => {
-  const Client = sequelize.define('Client', {
+  const Setting = sequelize.define('Setting', {
     id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
     uuid: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4 },
     createdAt: { field: 'created_at', type: DataTypes.DATE, defaultValue: DataTypes.NOW, allowNull: false },
@@ -11,33 +10,31 @@ export default (sequelize) => {
     updatedById: { field: 'updated_by_id', type: DataTypes.BIGINT, allowNull: false },
     deletedAt: { field: 'deleted_at', type: DataTypes.DATE, allowNull: true },
     deletedById: { field: 'deleted_by_id', type: DataTypes.BIGINT, allowNull: true },
-    name: { field: 'name', type: DataTypes.STRING, allowNull: false, unique: true },
-    code: { field: 'code', type: DataTypes.STRING, allowNull: false, unique: true },
-    accessCode: { field: 'access_code', type: DataTypes.STRING, allowNull: false, unique: true },
-    isActive: { field: 'is_active', type: DataTypes.BOOLEAN, defaultValue: true },
-    status: { field: 'status', type: DataTypes.ENUM(...clientStatusValues), allowNull: false },
+    key: { field: 'key', type: DataTypes.STRING, allowNull: false, unique: true },
+    value: { field: 'value', type: DataTypes.JSONB, allowNull: false, unique: true },
+    description: { field: 'description', type: DataTypes.STRING, allowNull: true },
   }, {
-    tableName: 'clients',
+    tableName: 'settings',
     schema: 'ia_desk',
     timestamps: false,
   });
 
-  Client.associate = (models) => {
-    Client.belongsTo(models.User, {
+  Setting.associate = (models) => {
+    Setting.belongsTo(models.User, {
       foreignKey: 'createdById',
       as: 'createdBy',
     });
 
-    Client.belongsTo(models.User, {
+    Setting.belongsTo(models.User, {
       foreignKey: 'updatedById',
       as: 'updatedBy',
     });
 
-    Client.belongsTo(models.User, {
+    Setting.belongsTo(models.User, {
       foreignKey: 'deletedById',
       as: 'deletedBy',
     });
   };
 
-  return Client;
+  return Setting;
 };
