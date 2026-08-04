4a98491d28f9867413d55998936b65bb200d959f
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-04 15:23:23 -0300

    fix: rename device property to deviceToken

--- backend/dto/session.dto.js
+++ backend/dto/session.dto.js
@@ -4,7 +4,7 @@ export class SessionResponse {
   constructor(session) {
     this.authorizationToken = session.authorizationToken;
     this.autoLoginToken = session.autoLoginToken;
-    this.device = session.device.token;
+    this.deviceToken = session.device.token;
     this.user = new UserMinDTO(session.user);
     this.roles = session.roles?.map(r => r.name);
     this.permissions = session.permissions?.map(p => p.name);
