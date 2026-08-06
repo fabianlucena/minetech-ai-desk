25832e478da8f93fa5262eb3b116e880fb9493f8
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-06 16:44:07 -0300

    feat: implement conversation management with normalization and API integration

--- mobile/app/screens/ConversationsScreen.jsx
+++ mobile/app/screens/ConversationsScreen.jsx
@@ -1,7 +1,24 @@
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { error } from '../components/Toast';
+import useConversation from '../services/useConversation';
 
 export default function ConversationsScreen() {
+  const { getConversations } = useConversation();
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    getConversations()
+      .then(setConversations)
+      .catch(err => error('Error al cargar las conversaciones:', err));
+  }, []);
+
   return <View>
     <Text>Pantalla de Conversaciones</Text>
+    <FlatList
+      data={conversations}
+      keyExtractor={(item) => item.uuid}
+      renderItem={({ item }) => <Text>{item.lastMessageAt.toString()}</Text>}
+    />
   </View>;
 }
