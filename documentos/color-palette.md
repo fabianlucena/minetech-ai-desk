14c75e8aae7c632ab1b069ac4bcabc46c5bba2c2
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-14 14:55:53 -0300

    fix: ajustar formato de importaciones en Menu.jsx y agregar paleta de colores

---
+++ documentos/color-palette.md
@@ -0,0 +1,14 @@
+# Intentar
+
+Combinación 1:
+  1 #D8B128
+  2 #C94132
+  3 #888888
+
+Combinación 2:
+  1 #BE9B23
+  2 #777777
+  3 #B1392C
+
+Resaltado del botón: #5C616A
+Fondo del botón: #d98128
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, List, ListItemButton, ListItemText, ListItemIcon } from '@mui/material';
-import { Link, useLocation  } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useGlobal from '../contexts/useGlobal.jsx';
 import { useRoutes } from '../routes.jsx';
 
